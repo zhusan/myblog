21db5e1e4a7f23d10cd2c013569472125685cbd6
zhusan <zhusan333@163.com>
2013-05-24 23:40:47 +0800

    add layout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,17 @@
 class UserController < ApplicationController
   def login
+    puts request.post?,"111111111111111111111111111111"
+    
+    return unless request.post?
+    user = User.auth_login(params[:login],params[:password])
+    if user
+      session[:user] = user.id
+      flash[:notice] = "登录成功！"
+      redirect_to :action => :index
+    else
+      flash[:notice] = "账号或密码错误！"
+      redirect_to :action => :login
+    end
   end
 
- def create
-  user = User.auth_login(params[:login],params[:password])
-  if user
-    session[:user] = user.id
-    flash[:notice] = "登录成功！"
-    redirect_to :action => :index
-  else
-    flash[:notice] = "账号或密码错误！"
-    redirect_to :action => :login
-  end
- end 
 end
